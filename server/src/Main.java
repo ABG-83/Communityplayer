a992819c15bec55142eb2c96d1edc69db39ce171
ABG-83 <148800858+ABG-83@users.noreply.github.com>
2024-03-08 19:53:06 -0600

    Primero commit

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,5 +1,47 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private int port;
+    private int backlog;
+
+    public Main (int port, int backlog) {
+        this.port = port;
+        this.backlog = backlog;
     }
+
+    public void startServer() {
+        try (ServerSocket serverSocket = new ServerSocket(port, backlog)) {
+
+            System.out.println("Started Listening for clients");
+            while (true) {
+
+                // take input and output streams
+                try (Socket client = serverSocket.accept();
+                     Scanner scanner = new Scanner(client.getInputStream());
+                     PrintWriter pw = new PrintWriter(client.getOutputStream(), true)) {
+                    String dataFromClient = scanner.nextLine();
+
+                    String response = getResponse(dataFromClient);
+                    pw.write(response);
+
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private String getResponse(String dataFromClient) {
+        return dataFromClient.toUpperCase();
+    }
+
 }
---
+++ server/src/Server.java
@@ -0,0 +1,9 @@
+public class Server {
+
+    public static void main(String args[]){
+        Main customServer = new Main(1234, 10);
+
+        customServer.startServer();
+
+    }
+}
